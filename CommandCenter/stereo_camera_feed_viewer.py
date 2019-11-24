d9f470f7dac11b94cb88951225b8a93cb96a2eb1
Nicholas Kajoh <kajohterna@gmail.com>
2019-11-24 14:40:04 +0100

    Refactor code use Redis for messaging instead of ROS

--- CommandCenter/stereo_camera_feed_viewer.py
+++ CommandCenter/stereo_camera_feed_viewer.py
@@ -1,33 +1,44 @@
-from __future__ import print_function
-import roslibpy
-from cv_bridge import CvBridge, CvBridgeError
+import redis
+import numpy as np
+import pickle
+import time
 import cv2
 
 
-def main(client):
-    bridge = CvBridge()
+try:
+    r = redis.Redis()
+except redis.ConnectionError as e:
+    print(e)
 
-    def display_image(image_message, window_name):
-        try:
-            image = bridge.imgmsg_to_cv2(image_message, 'rgb8')
-        except CvBridgeError as e:
-            print(e)
+def display_image(image_message, window_name):
+    if type(image_message['data']) is not int:
+        image = pickle.loads(image_message['data'])
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
         image = cv2.resize(image, (854, 480))
         cv2.imshow(window_name, image)
         cv2.waitKey(1)
 
-    # print(client.get_topics(None))
+def main():
+    cam_left = r.pubsub()
+    cam_left.subscribe('cam_left')
 
-    cam_left_listener = roslibpy.Topic(client, '/camera_left', 'sensor_msgs/Image')
-    cam_left_listener.subscribe(lambda msg: display_image(msg, 'Left cam'))
-    # print(cam_left_listener.is_subscribed)
+    cam_right = r.pubsub()
+    cam_right.subscribe('cam_right')
 
-    cam_right_listener = roslibpy.Topic(client, '/camera_right', 'sensor_msgs/Image')
-    cam_right_listener.subscribe(lambda msg: display_image(msg, 'Right cam'))
+    try:
+        while True:
+            cam_left_message = cam_left.get_message()
+            cam_right_message = cam_right.get_message()
 
-try:
-    ros_client = roslibpy.Ros(host='localhost', port=9090)
-    ros_client.on_ready(lambda: main(ros_client))
-    ros_client.run_forever()
-except KeyboardInterrupt:
-    ros_client.terminate()
+            if cam_left_message:
+                display_image(cam_left_message, 'Cam left')
+
+            if cam_right_message:
+                display_image(cam_right_message, 'Cam right')
+
+            time.sleep(0.03)
+    except KeyboardInterrupt:
+        pass
+
+if __name__ == '__main__':
+    main()
---
+++ Vehicle/carla_video_capture.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+
+import glob
+import os
+import sys
+
+try:
+    sys.path.append(glob.glob('%s/dist/carla-*%d.%d-%s.egg' % (
+        os.path.dirname(os.path.realpath(__file__)),
+        sys.version_info.major,
+        sys.version_info.minor,
+        'win-amd64' if os.name == 'nt' else 'linux-x86_64'))[0])
+except IndexError:
+    pass
+
+import carla
+
+import redis
+import numpy as np
+import time
+import pickle
+
+
+try:
+    r = redis.Redis()
+except redis.ConnectionError as e:
+    print(e)
+
+def publish(carla_image, topic):
+    image = np.frombuffer(carla_image.raw_data, dtype=np.dtype('uint8'))
+    image = np.reshape(image, (carla_image.height, carla_image.width, 4))
+    image = image[:, :, :3]
+    image = image[:, :, ::-1]
+    image_message = pickle.dumps(image, protocol=0)
+    r.publish(topic, image_message)
+
+
+def carla_video_capture():
+    # create simulator client
+    client = carla.Client('localhost', 2000)
+    client.set_timeout(5.0)
+
+    # get simulator world
+    world = client.get_world()
+
+    # get camera sensors
+    cam_left = None; cam_right = None
+    for actor in world.get_actors():
+        if actor.attributes.get('role_name') == 'autonomous_car_camera_left':
+            cam_left = actor
+
+        if actor.attributes.get('role_name') == 'autonomous_car_camera_right':
+            cam_right = actor
+    if cam_left is None or cam_right is None:
+        raise Exception('Could not find camera sensors')
+
+
+    cam_left.listen(lambda carla_image: publish(carla_image, 'cam_left'))
+    cam_right.listen(lambda carla_image: publish(carla_image, 'cam_right'))
+
+    print('Streaming video from cameras')
+
+    try:
+        while True:
+            time.sleep(5)
+    except KeyboardInterrupt:
+        pass
+
+if __name__ == '__main__':
+    carla_video_capture()
